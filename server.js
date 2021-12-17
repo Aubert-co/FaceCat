c01f4da17691f084e8f2d781888d1c5c9d8730e0
Black <53127242+Aubert-co@users.noreply.github.com>
2021-12-16 23:21:53 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const expressSession = require('express-session')
 const app = express()
-const route =require('./route/index')
+const route =require('./controller/route')
 
 app.use(express.json())
 app.use(expressSession({
