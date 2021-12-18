a99074f40052d6fe8c747bb644af44fd1edc9bc7
Black <53127242+Aubert-co@users.noreply.github.com>
2021-12-17 23:33:42 -0300

    Update login.js

--- controller/login.js
+++ controller/login.js
@@ -1,8 +1,21 @@
+const bcript = require('bcrypts')
+const Users = require('../model/users')
 module.exports = async function ApiLogin(req,res){
-    const {name,password} = req.body
+    try{
+    const {email,password} = req.body
 
-    if(!name || !password)return res.status(401).send({msg:'invalid datas'})
+    if(!email || !password)return res.status(401).send({msg:'invalid datas'})
 
-    if(typeof name !== 'string')return res.status(401).send({msg:'invalid name'})
+    if(typeof email !== 'string')return res.status(401).send({msg:'invalid email'})
+
+    const findEmail = await Users.find({where:{
+        email
+    }})
+    if(findEmail.lenght === 1){
+        
+    }
     res.status(200).send({msg:'sucess'})
+}catch(err){
+    res.status(501).send({msg:'something went wrong'})
+}
 }
