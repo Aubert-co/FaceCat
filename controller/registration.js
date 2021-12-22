43eae00feea4f567f3e4b7b1d05ec37e5ccbe90e
Black <53127242+Aubert-co@users.noreply.github.com>
2021-12-21 23:23:17 -0300

    Update registration.js

--- controller/registration.js
+++ controller/registration.js
@@ -1,8 +1,12 @@
+const DataUsers = require('../model/users')
 module.exports = async function ApiRegistration(req,res){
     const {name,password} = req.body
 
     if(!name || !password)return res.status(401).send({msg:'invalid datas'})
 
     if(typeof name !== 'string')return res.status(401).send({msg:'invalid name'})
+
+
+
     res.status(200).send({msg:'sucess'})
 }
