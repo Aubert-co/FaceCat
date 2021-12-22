52c1bd8ada1b2be92bff5a61f6265d961aec77cf
Black <53127242+Aubert-co@users.noreply.github.com>
2021-12-22 18:26:39 -0300

    Update route.js

--- controller/route.js
+++ controller/route.js
@@ -3,7 +3,7 @@ const ApiIndex = require('./index')
 const ApiProfile = require('./profile')
 const ApiLogin = require('./login')
 const ApiRegistration =require('./registration')
-
+const Middleware = require('../midlleware/index')
 route
 .get('/',ApiIndex)
 .get('/apiprofile',ApiProfile)
