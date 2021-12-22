99615e04fc1b1d84138097e1e632596e4439c858
Black <53127242+Aubert-co@users.noreply.github.com>
2021-12-21 23:24:00 -0300

    Update profile.js

--- controller/profile.js
+++ controller/profile.js
@@ -1,4 +1,7 @@
 module.exports = async function ApiProfile(req,res){
-    
+    try{
     res.status(200).send({msg:'sucess',data:[]})
+    }catch(err){
+        res.send(501).send({msg:'something went wrong'})
+    }
 }
