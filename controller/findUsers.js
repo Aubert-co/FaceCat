61a73f39c5d3e54afa337f718e923909dbc989eb
Black <53127242+Aubert-co@users.noreply.github.com>
2021-12-24 23:46:51 -0300

    Update findUsers.js

--- controller/findUsers.js
+++ controller/findUsers.js
@@ -1,5 +1,10 @@
+const Users = require('../model/users')
 module.exports = async function FindUsers(req,res){
-    const {name,id} = req.body
-
+    const {name} = req.body
+    Users.findAll({
+        where:{
+            name
+        }
+    })
     res.status(501).send({msg:'sucess',data:[]})
 }
