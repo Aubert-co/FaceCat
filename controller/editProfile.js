803279327a28d334574becf4c8cffd52007be33e
Black <53127242+Aubert-co@users.noreply.github.com>
2021-12-25 23:10:35 -0300

    Update editProfile.js

--- controller/editProfile.js
+++ controller/editProfile.js
@@ -1,6 +1,13 @@
+const Users = require('../model/users')
 module.exports = async function EditProfile(req,res){
-
-    const {img,name,password} =  req.body
+    try{
+    const {id,img,name,password} =  req.body
     
-    return res.status(200).send({msg:'sucess',data:[]})
+    Users.update(name,{
+        where:{id}
+    })
+    return res.status(200).send({msg:'sucess'})
+}catch(err){
+    res.status(501).send({msg:'something went wrong'})
+}
 }
