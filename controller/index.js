d89d86ddfe3befb6fd57838c881d590939dd28bc
Black <53127242+Aubert-co@users.noreply.github.com>
2021-12-25 23:11:43 -0300

    Update index.js

--- controller/index.js
+++ controller/index.js
@@ -1,4 +1,10 @@
+const Users = require('../model/users')
 module.exports = async function ApiIndex(req,res){
-    console.log(req.session.id)
-    res.status(200).send({msg:'hi'})
+    try{
+        const {userId:id} =  req.session.userId
+        const data = await Users.findOne({where:{id}})
+        res.status(200).send({msg:'sucess',data})
+    }catch(err){
+        res.status(501).send({msg:'something went wrong'})
+    }
 }
