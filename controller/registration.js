7b5ae8c81a4e15804ea90183e90abbade4977772
Black <53127242+Aubert-co@users.noreply.github.com>
2021-12-25 23:12:05 -0300

    Update registration.js

--- controller/registration.js
+++ controller/registration.js
@@ -1,18 +1,22 @@
 const DataUsers = require('../model/users')
 const bcrypt = require('bcrypt')
 module.exports = async function ApiRegistration(req,res){
-    const {name,password} = req.body
+    try{
+        const {name,password} = req.body
 
-    if(!name || !password)return res.status(401).send({msg:'invalid datas'})
+        if(!name || !password)return res.status(401).send({msg:'invalid datas'})
 
-    if(typeof name !== 'string')return res.status(401).send({msg:'invalid name'})
+        if(typeof name !== 'string')return res.status(401).send({msg:'invalid name'})
 
-    var passwordHash
-    DataUsers.create({
-        name:name,
-        password:passwordHash
-    })
+        var passwordHash
+        const registration = await DataUsers.create({
+            name:name,
+            password:passwordHash
+        })
 
 
-    res.status(200).send({msg:'sucess'})
+        res.status(200).send({msg:'sucess'})
+    }catch(err){
+        res.status(501);send({msg:'something went wrong'})
+    }
 }
