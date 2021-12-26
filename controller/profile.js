c9ac9c351fb310658d203a2ce5c6ca76678446c6
Black <53127242+Aubert-co@users.noreply.github.com>
2021-12-25 23:11:54 -0300

    Update profile.js

--- controller/profile.js
+++ controller/profile.js
@@ -1,7 +1,9 @@
+const Users = require('../model/users')
 module.exports = async function ApiProfile(req,res){
     try{
         const {id,name} = req.body
-        res.status(200).send({msg:'sucess',data:[]})
+        const data = await Users.findOne({where:{id}})
+        res.status(200).send({msg:'sucess',data:data})
     }catch(err){
         res.send(501).send({msg:'something went wrong'})
     }
