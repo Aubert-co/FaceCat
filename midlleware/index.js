e1f5122561c863829c9b7c72e76fd79d86cbc6f3
Black <53127242+Aubert-co@users.noreply.github.com>
2021-12-26 23:15:28 -0300

    Update index.js

--- midlleware/index.js
+++ midlleware/index.js
@@ -1,14 +1,16 @@
-const jsonwebtoken = require('jsonwebtoken')
+const jwt = require('jsonwebtoken')
 
 module.exports = async function middlewareApi(req,res,next){
-    if(!req.session.token)return res.status(403).send({msg:'undefined token'})
+    const token = req.headers['x-api-token']
 
-    const token  = req.session
-
-    const verifyToken = await jsonwebtoken.verify(token,'secret')
-
-    if(!verifyToken)return res.status(403).send({msg:'invalid token'})
+    if(!token)return res.status(403).send({msg:'undefined token'})
 
     
-    next()
+
+    const verifyToken = jwt.verify(token,'secret',(err,decoded)=>{
+        if(err)return res.status(401).send({msg:'Not authorized'})
+
+        next()
+    })
+
 }
