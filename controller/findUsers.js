922908b57587722e7d3cf3fbbe72e2706a0babb2
Black <53127242+Aubert-co@users.noreply.github.com>
2021-12-27 22:17:39 -0300

    Update findUsers.js

--- controller/findUsers.js
+++ controller/findUsers.js
@@ -3,7 +3,8 @@ module.exports = async function FindUsers(req,res){
     try{
         const {name} = req.body
         const data =  await Users.findAll({ where:{name}})
-        res.status(501).send({msg:'sucess',data})
+        const {photo,photo_image} = data
+        res.status(200).send({msg:'sucess',data:{photo}})
     }catch(err){
         res.status(501).send({msg:'something went wrong'})
     }   
