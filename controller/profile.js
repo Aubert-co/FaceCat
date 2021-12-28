5557738ebbec87ebfac492b62de0917aa0c62e04
Black <53127242+Aubert-co@users.noreply.github.com>
2021-12-27 22:18:45 -0300

    Update profile.js

--- controller/profile.js
+++ controller/profile.js
@@ -1,9 +1,12 @@
 const Users = require('../model/users')
 module.exports = async function ApiProfile(req,res){
     try{
-        const {id,name} = req.body
+        const token = req.headers['x-api-token']
+        const {jwt:json}= jwt.decode(token,'secret')
+        const {name,id} = json
         const data = await Users.findOne({where:{id}})
-        res.status(200).send({msg:'sucess',data:data})
+        const {photo} = data
+        res.status(200).send({msg:'sucess',data:{photo}})
     }catch(err){
         res.send(501).send({msg:'something went wrong'})
     }
